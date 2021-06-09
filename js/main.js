afdd82c613740c2b864ed9bfa110a9bbcc1b3039
YouJeen-ru <1.rouch@mail.ru>
2021-06-09 18:18:54 +0300

    adaptive layout completed

--- js/main.js
+++ js/main.js
@@ -3,6 +3,35 @@ $(function(){
         arrows: false,
         dots: true,
         slidesToShow: 3,
+        responsive: [
+            {
+                breakpoint: 841,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    dots: true
+                },
+
+            },
+            {
+                breakpoint: 601,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: true
+                },
+
+            },
+            {
+                breakpoint: 339,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: false
+                },
+
+            },
+    ]
 
     })
 });
